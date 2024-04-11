61fd5dbac6f290ee583a314fb6b4fff37851039b
Noel Lopes <noel@ipg.pt>
2024-04-11 15:47:41 +0100

    Read user guess

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Guess the number!");
+
+    let guess = read_guess();
+
+    println!("You guessed: {guess}");
+}
+
+fn read_guess() -> String {
+    println!("Please input your guess.");
+    
+    let mut guess = String::new();
+    
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read line");
+
+    guess
 }
