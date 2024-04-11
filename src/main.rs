e1378d7c05372ff687c5d26949609ac09679d225
Noel Lopes <noel@ipg.pt>
2024-04-11 15:56:04 +0100

    Generate a random number

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,23 @@
 use std::io;
 
+use rand::{thread_rng, Rng};
+
 fn main() {
     println!("Guess the number!");
+
+    let _secret_number = generate_random_number();
 
     let guess = read_guess();
 
     println!("You guessed: {guess}");
+}
+
+fn generate_random_number() -> u32 {
+    let secret_number = thread_rng().gen_range(1..=100);
+
+    println!("The secret number is: {secret_number}");
+
+    secret_number
 }
 
 fn read_guess() -> String {
